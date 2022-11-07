97663d33cfa8ac01e1e876ca7880219463623fcb
+ <+>
2022-11-07 15:03:31 +0300

    Task Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
 puts "🌱 Seeding data..."
 
 # place your seeds here
+Category.create(name: "Work")
+Category.create(name: "Personal")
+Category.create(name: "Chores")
+Category.create(name: "Errands")
+Category.create(name: "Hobbies")
+
+# create todolists
+Category.all.each do |category|
+  5.times do
+    TodoList.create(
+      title: Faker::Lorem.sentence(word_count: 3),
+      description: Faker::Lorem.paragraph(sentence_count: 2),
+      category_id: category.id,
+      status: false
+    )
+  end
+end
 
 puts "🌱 Done seeding!"
